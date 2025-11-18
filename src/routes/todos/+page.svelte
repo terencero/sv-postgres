23e74ef181709c89d66b42da2f1f06f9d44c1b87
Terence Ro <rowyourboat83@gmail.com>
2025-11-18 09:20:41 -0600

    saving point: creating a todo page and server actions

--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -1,1 +1,31 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	import Form, { type FormFields } from "$lib/components/Form.svelte";
+	import type { HTMLFormAttributes } from "svelte/elements";
+
+  let showForm = $state(false);
+
+  const method: HTMLFormAttributes['method'] = 'POST';
+  // TODO: create a server action
+  const action = `${page.route.id}?`;
+  const formFields: FormFields = {
+    action,
+    method,
+    submitText: 'Add a Todo',
+    fields: [],
+  };
+</script>
+
 <h1>Todos</h1>
+
+<button onclick={() => showForm = !showForm}>
+  {#if showForm}
+    Hide Form
+  {:else}
+    Add a Todo
+  {/if}
+</button>
+
+{#if showForm}
+  <Form {...formFields} />
+{/if}
