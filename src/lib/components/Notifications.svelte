47f0a2f59c67d808a0aa390dffcde97bb47cf08f
Terence Ro <rowyourboat83@gmail.com>
2025-11-30 14:07:04 -0600

    change the getTodosByUpcoming function to query todos by lte; TODO: why is the todos query only returning todos for one pet?

--- src/lib/components/Notifications.svelte
+++ src/lib/components/Notifications.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import type { SelectTodos } from "$lib/server/db/schema";
+
 // get a list of todos with due dates
 // sort them by closest upcoming
 // flag ones that should trigger an animation (default to on day for now)
-  let { data } = $props();
+  interface Props {
+    data: SelectTodos[];
+  }
+  let { data }: Props = $props();
 </script>
 
 <h2>Notifications</h2>
+
+<!-- <form action="?/"> -->
+<!--    -->
+<!-- </form> -->
 <ul>
   {#each data as todo (todo.id)}
     <li>Title: {todo.title}, Due Date: {todo.dueDate}</li>
