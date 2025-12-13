7823268fdfcdd7b451c0d4114bfd9b23beb6142e
Terence Ro <rowyourboat83@gmail.com>
2025-12-13 12:14:15 -0600

    fix notifications component expecting pet but it being possibly null;

--- src/lib/components/Notifications.svelte
+++ src/lib/components/Notifications.svelte
@@ -4,7 +4,7 @@
 // get a list of todos with due dates
 // sort them by closest upcoming
 // flag ones that should trigger an animation (default to on day for now)
-  type TodosWithPet = SelectTodos & { pet: SelectPetProfiles };
+  type TodosWithPet = SelectTodos & { pet: SelectPetProfiles | null };
   interface Props {
     data: TodosWithPet[];
   }
@@ -15,6 +15,6 @@
 
 {#each data as todo (todo.id)}
   <div>
-      Title: {todo.title}, Due Date: {todo.dueDate}, For Pet: {todo.pet.name}
+      Title: {todo.title}, Due Date: {todo.dueDate}, For Pet: {todo.pet?.name}
   </div>
 {/each}
