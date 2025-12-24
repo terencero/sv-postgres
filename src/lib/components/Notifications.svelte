28fc172056a07383a1d5f9cd34203779c521a101
Terence Ro <rowyourboat83@gmail.com>
2025-12-24 09:29:19 -0600

    following this eslint plugin issue[https://github.com/sveltejs/eslint-plugin-svelte/issues/1353] about the strictness of the recommended eslint rules about using resolve and conflicts with how svelte uses template hrefs... making it warning for now so that it reminds me to check on it periodically...

--- src/lib/components/Notifications.svelte
+++ src/lib/components/Notifications.svelte
@@ -4,17 +4,19 @@
 // get a list of todos with due dates
 // sort them by closest upcoming
 // flag ones that should trigger an animation (default to on day for now)
-  type TodosWithPet = SelectTodos & { pet: SelectPetProfiles | null };
+  type TodosWithPet = SelectTodos & { pet: SelectPetProfiles };
   interface Props {
-    data: TodosWithPet[];
+    todos: TodosWithPet[];
   }
-  let { data }: Props = $props();
+  let { todos }: Props = $props();
 </script>
 
 <h2>Notifications</h2>
 
-{#each data as todo (todo.id)}
+{#each todos as todo (todo.id)}
   <div>
-      Title: {todo.title}, Due Date: {todo.dueDate}, For Pet: {todo.pet?.name}
+    <a href={`/todos#${todo.id}-${todo.title}`}>
+      Title: {todo.title}, Due Date: {todo.dueDate}, For Pet: {todo.pet.name}
+    </a>
   </div>
 {/each}
